867f6e602b0f2d5e9026840e049b2b97adc1a1de
Bennett Batzli <bennett.batzli@gmail.com>
2016-03-28 15:02:31 -0500

    base logic for creating lesson plan object is in place

--- public/scripts/controllers/lessonPlanController.js
+++ public/scripts/controllers/lessonPlanController.js
@@ -2,14 +2,28 @@ myApp.controller('LessonPlanController', ['$scope', '$http', 'PassportFactory', 
 
   console.log('lesson plan controller');
 
+  var typeBoolean;
+
   $scope.submitLesson = function() {
+
+    console.log('checked', $scope.required_materials);
+
+    if ($scope.type_selector == "resource") {
+      typeBoolean = true;
+    } else {
+      typeBoolean = false;
+    }
 
     var lessonPlan = {
       author: $scope.lesson_author,
       title: $scope.lesson_title,
-      materials: $scope.lesson_materials,
-      text: $scope.lesson_text,
-      type: $scope.type_selector
+      lesson_plan: {
+        materials: $scope.lesson_materials,
+        text: $scope.lesson_text,
+      },
+      materials: $scope.required_materials,
+      resource: typeBoolean
+
       // tags: $scope.tag
     };
     console.log('lessonplan', lessonPlan);

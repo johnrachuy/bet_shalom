965f84379989fd9696e37c5bbc415ec82ce643b6
Bennett Batzli <bennett.batzli@gmail.com>
2016-03-28 12:02:35 -0500

    added submit function and publish button

--- public/scripts/controllers/lessonPlanController.js
+++ public/scripts/controllers/lessonPlanController.js
@@ -2,4 +2,17 @@ myApp.controller('LessonPlanController', ['$scope', '$http', 'PassportFactory', 
 
   console.log('lesson plan controller');
 
+  $scope.submitLesson = function() {
+
+    var lessonPlan = {
+      author: $scope.lesson_author,
+      title: $scope.lesson_title,
+      materials: $scope.lesson_materials,
+      text: $scope.lesson_text,
+      type: $scope.type_selector
+      // tags: $scope.tag
+    };
+    console.log('lessonplan', lessonPlan);
+  };
+
 }]);

1d55ea37757cd96f090a880b7aeca4ac6f39689f
John Rachuy II <john.rachuy@gmail.com>
2016-04-01 15:47:38 -0500

    Added search function - working

---
+++ routes/tag_search.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var path = require('path');
+var connection = require('../modules/connection');
+var pg = require('pg');
+
+//from createUserController
+router.get('/:selectedTag', function(req, res) {
+    var results = [];
+
+    pg.connect(connection, function(err, client, done) {
+        var query = client.query('SELECT lesson.title, lesson.author FROM lesson JOIN lesson_tag ON lesson.lesson_id = lesson_tag.fk_lesson_id JOIN tag ON lesson_tag.fk_tag_id = tag.tag_id WHERE tag.tag_id = ($1)',
+            [req.params.selectedTag]);
+
+        //Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        //close connection
+        query.on('end', function() {
+            done();
+
+            return res.json(results);
+        });
+
+        if(err) {
+            console.log(err);
+        }
+    });
+});
+
+module.exports = router;

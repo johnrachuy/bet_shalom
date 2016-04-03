dfe012ae645e665607e1cff840713fbc0e556a5d
John Rachuy II <john.rachuy@gmail.com>
2016-04-03 15:05:53 -0500

    Updated the search.html, added published column to db and added that published datestamp in lesson.js - buttons on search page not working yet

--- public/scripts/controllers/searchController.js
+++ public/scripts/controllers/searchController.js
@@ -1,9 +1,11 @@
-myApp.controller('SearchController', ['$scope', 'PassportFactory', '$http', '$window', function($scope, PassportFactory, $http, $window) {
+myApp.controller('SearchController', ['$scope', 'PassportFactory', 'DataFactory', '$http', function($scope, PassportFactory, DataFactory, $http) {
 
+    //Populates the drop-down menus
     $http.get('/tags').then(function(response) {
         $scope.keyTags = response.data;
     });
 
+    //Tied to a ng-change on the first drop-down menu
     $scope.searchTag = function() {
         $http.get('/tag_search/'+ $scope.selectedTag).then(function(response) {
             $scope.viewLesson = response.data;

574e3b5dca161651bd1fa074e90216e1d31b4f6d
Joshua Sternberg <sterny5000@yahoo.com>
2016-04-02 21:30:15 -0500

    routes from admin and teacher dahshboards are set up and passing values back and forth. Set up default for radio buttons on lesson plan input. added code comments for lesson plan controller, lesson plan html, dataFactory, admin dashboard controller and teacher dashboard controllers.

---
+++ routes/admin_dashboard.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var connection = require('../modules/connection');
+var pg = require('pg');
+
+router.get('/',  function(req, res) {
+    var results =[];
+    pg.connect(connection, function(err, client, done) {
+        var query = client.query('SELECT lesson_id, author, title, materials, status FROM lesson');
+            //'WHERE status = ($1, $2)',
+            //['needs review', 'submitted']);
+
+        //Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+            console.log(results);
+        });
+
+        //close connection
+        query.on('end', function() {
+            done();
+
+            return res.json(results);
+        });
+
+        if(err) {
+            console.log(err);
+        }
+    });
+});
+
+module.exports = router;

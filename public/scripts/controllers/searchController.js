71efd3134a18ba4bf12c9a254f0c3cfb84aae304
John Rachuy II <john.rachuy@gmail.com>
2016-04-03 16:01:32 -0500

    Got the select buttons on search.html routing selected lesson plan to lesson_plan.html

--- public/scripts/controllers/searchController.js
+++ public/scripts/controllers/searchController.js
@@ -1,4 +1,21 @@
-myApp.controller('SearchController', ['$scope', 'PassportFactory', 'DataFactory', '$http', function($scope, PassportFactory, DataFactory, $http) {
+myApp.controller('SearchController', ['$scope', 'PassportFactory', 'DataFactory', '$location', '$http', function($scope, PassportFactory, DataFactory, $location, $http) {
+
+    $scope.passportFactory = PassportFactory;
+    $scope.dataFactory = DataFactory;
+    $scope.loggedInUser = {};
+
+    $scope.loggedInUser = $scope.passportFactory.factoryLoggedInUser();
+
+    //Function to check the user and re-route them if they are not validated
+    validateUser();
+
+    function validateUser() {
+        if($scope.loggedInUser.role == 'admin') {
+
+        } else {
+            $location.path('/home');
+        }
+    }
 
     //Populates the drop-down menus
     $http.get('/tags').then(function(response) {
@@ -9,8 +26,17 @@ myApp.controller('SearchController', ['$scope', 'PassportFactory', 'DataFactory'
     $scope.searchTag = function() {
         $http.get('/tag_search/'+ $scope.selectedTag).then(function(response) {
             $scope.viewLesson = response.data;
+            console.log($scope.viewLesson);
             $scope.selectedName = null;
         });
+    };
+
+    //Function to reroute the user to the lesson plan controller
+    $scope.editClickedLesson = function(index){
+        $scope.dataFactory.factoryStoredLessonId = $scope.viewLesson[index].lesson_id;
+        //$scope.dataFactory.factoryStoredLessonId = id.lesson_id;
+        $scope.dataFactory.factoryLessonViewState = true;
+        $location.path('/lesson_plan');
     };
 
     console.log('Search Controller');

bfb7380c66dff1684e2fb95ca9c9c564f8cf5bb8
John Rachuy II <john.rachuy@gmail.com>
2016-04-18 15:10:43 -0500

    Updated connection string

--- modules/connection.js
+++ modules/connection.js
@@ -1,9 +1,9 @@
 var connectionString = '';
 
 if(process.env.DATABASE_URL !== undefined) {
-    connectionString = process.env.DATABASE_URL + 'ssl';
+    connectionString = process.env.DATABASE_URL + '?ssl=true';
 } else {
-    connectionString = 'postgres://localhost:5432/bet_shalom';
+    connectionString = 'postgres://dqqhueitqjswre:wgvCElpoXCoYBkANPhDXVYxjLp@ec2-23-21-215-184.compute-1.amazonaws.com:5432/dfdvljek5oqofi';
 }
 
 module.exports = connectionString;

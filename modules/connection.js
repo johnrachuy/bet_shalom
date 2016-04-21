4168f445e6a2725c79ff7f90a469c35a0c86fb49
John Rachuy II <john.rachuy@gmail.com>
2016-04-21 11:17:33 -0500

    Switched connection string back to Heroku

--- modules/connection.js
+++ modules/connection.js
@@ -1,23 +1,23 @@
 //connectionsString for Heroku database
-//var connectionString = '';
-//
-//if(process.env.DATABASE_URL !== undefined) {
-//    connectionString = process.env.DATABASE_URL + '?ssl=true';
-//} else {
-//    connectionString = 'postgres://dqqhueitqjswre:wgvCElpoXCoYBkANPhDXVYxjLp@ec2-23-21-215-184.compute-1.amazonaws.com:5432/dfdvljek5oqofi';
-//}
-//
-//module.exports = connectionString;
+var connectionString = '';
+
+if(process.env.DATABASE_URL !== undefined) {
+    connectionString = process.env.DATABASE_URL + '?ssl=true';
+} else {
+    connectionString = 'postgres://dqqhueitqjswre:wgvCElpoXCoYBkANPhDXVYxjLp@ec2-23-21-215-184.compute-1.amazonaws.com:5432/dfdvljek5oqofi';
+}
+
+module.exports = connectionString;
 
 
 
 //connectionsString for localhost database
-var connectionString = '';
-
-if(process.env.DATABASE_URL !== undefined) {
-    connectionString = process.env.DATABASE_URL + 'ssl';
-} else {
-    connectionString = 'postgres://localhost:5432/bet_shalom';
-}
-
-module.exports = connectionString;
+//var connectionString = '';
+//
+//if(process.env.DATABASE_URL !== undefined) {
+//    connectionString = process.env.DATABASE_URL + 'ssl';
+//} else {
+//    connectionString = 'postgres://localhost:5432/bet_shalom';
+//}
+//
+//module.exports = connectionString;
